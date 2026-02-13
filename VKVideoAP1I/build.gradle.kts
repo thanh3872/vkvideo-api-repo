22a60d7cb33744a79a470a6f1852a8f616f5ba4f
thanh3872 <157853088+thanh3872@users.noreply.github.com>
2026-02-13 15:30:16 +0700

    Configure plugin build

---
+++ VKVideoAP1I/build.gradle.kts
@@ -0,0 +1,13 @@
+
+    plugins {
+        id("com.android.library")
+        kotlin("android")
+        id("com.lagradost.cloudstream3.gradle")
+    }
+    cloudstream {
+        setDisplayName("VKVideoAP1I")
+        setDescription("VK Video extension for Cloudstream")
+        setAuthor("thanh3872")
+        setVersion(1)
+    }
+    
